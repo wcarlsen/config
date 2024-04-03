fdaa14c796affac41529096c7e19a575f7e37c36
Willi Carlsen <carlsenwilli@gmail.com>
2024-04-03 18:39:33 +0200

    Testing docker on lxc
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- systems/nixos-lxc/configuration.nix
+++ systems/nixos-lxc/configuration.nix
@@ -10,5 +10,28 @@
     ./../../modules/nixos/virtualization/docker.nix
   ];
 
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      pi-hole = {
+        image = "pihole/pihole:latest";
+        ports = [
+          "53:53/tcp"
+          "53:53/udp"
+          # "67:67/udp" # Only required if you are using Pi-hole as your DHCP server
+          "80:80/tcp"
+        ];
+        environments = {
+          TZ = "Europe/Copenhagen";
+        };
+        volumes = [
+          "./etc-pihole:/etc/pihole"
+          "./etc-dnsmasq.d:/etc/dnsmasq.d"
+        ];
+        autoStart = true;
+      };
+    };
+  };
+
   system.stateVersion = "23.11"; # Did you read the comment?s
 }
