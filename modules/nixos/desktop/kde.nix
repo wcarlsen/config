9404518dc76c34317c264355b9005f6815fee6f1
Willi Carlsen <carlsenwilli@gmail.com>
2024-05-10 13:53:32 +0200

    Remove unused KDE specific packages and flake update
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- modules/nixos/desktop/kde.nix
+++ modules/nixos/desktop/kde.nix
@@ -1,8 +1,16 @@
+{ pkgs, ...}:
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the KDE Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmax11";
+
+  # Exclude unused kde packages
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    kate
+    elisa
+  ];
 }
