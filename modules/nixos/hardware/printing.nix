d4a9ad97953feb80be2c5c5beeb41c07bf53411d
Willi Carlsen <carlsenwilli@gmail.com>
2024-09-09 12:02:33 +0200

    Made network printing work and update flake
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- modules/nixos/hardware/printing.nix
+++ modules/nixos/hardware/printing.nix
@@ -1,4 +1,11 @@
 {
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # Autodiscovery network printers
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
