283e4aa52a4558022f5a10b6c88f478c63f71f9f
Willi Carlsen <carlsenwilli@gmail.com>
2024-09-23 09:21:54 +0200

    Update flake and change kitty theme -> themeFile
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     # Nix packages
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.04";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
