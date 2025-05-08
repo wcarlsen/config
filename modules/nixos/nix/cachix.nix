f26ac8c8a3f3d0b6b6f5331f4e4df99db20920ac
Willi Carlsen <carlsenwilli@gmail.com>
2025-05-08 19:32:11 +0200

    Use helix cachix and update flake
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- modules/nixos/nix/cachix.nix
+++ modules/nixos/nix/cachix.nix
@@ -1,7 +1,13 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [cachix];
   nix.settings = {
-    substituters = ["https://nix-community.cachix.org"];
-    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://helix.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+    ];
   };
 }
