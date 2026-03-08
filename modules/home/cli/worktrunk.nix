c32f5bb39ed59d52e94f69848927aa0047832893
Willi Carlsen <carlsenwilli@gmail.com>
2026-03-08 20:22:40 +0100

    Update flake and fix worktrunk
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- modules/home/cli/worktrunk.nix
+++ modules/home/cli/worktrunk.nix
@@ -1,7 +1,17 @@
 {
+  lib,
   worktrunk,
   system,
   ...
 }: {
   home.packages = [worktrunk.packages.${system}.default];
+
+  programs.zsh.initContent = lib.mkOrder 2000 ''
+    eval "$(wt config shell init zsh)"
+  '';
+
+  xdg.configFile."worktrunk/config.toml".text = ''
+    skip-shell-integration-prompt = true
+    worktree-path = "../{{ branch | sanitize }}"
+  '';
 }
