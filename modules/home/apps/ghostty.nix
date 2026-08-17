87c2a3dd4c898c0277a8eea54e6a7df09d99ff46
Willi Carlsen <carlsenwilli@gmail.com>
2026-08-17 12:17:40 +0200

    Update flake

--- modules/home/apps/ghostty.nix
+++ modules/home/apps/ghostty.nix
@@ -9,15 +9,4 @@
       font-feature = "liga, dlig, calt";
     };
   };
-
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk
-      qt6Packages.fcitx5-configtool
-      qt6Packages.fcitx5-with-addons
-      fcitx5-m17n
-    ];
-  };
 }
